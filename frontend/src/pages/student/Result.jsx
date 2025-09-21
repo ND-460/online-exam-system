ddb9d9294c85ddae3f993510881534dd4430c087
ND-460 <22it460@bvmengineering.ac.in>
2025-09-21 13:54:47 +0530

    improved the ui of test

--- frontend/src/pages/student/Result.jsx
+++
@@ -1,8 +0,0 @@
-export default function Result() {
-  return (
-    <div className="min-h-screen bg-black p-8">
-      <h2 className="text-2xl font-bold text-blue-700 mb-4">Your Result</h2>
-      {/* TODO: Add result details here */}
-    </div>
-  );
-} 
